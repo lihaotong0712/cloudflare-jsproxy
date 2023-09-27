5a58eedc6a909bd524c416939b00b4bd0e9534a6
LiHaotong <3553645242@qq.com>
2023-09-27 23:23:33 +0800

    update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // sw.js
 // 监听 `install` 事件，回调中缓存所需文件
 self.addEventListener('install', e => {
-    console.log("1")
+    console.log("1");
     /* e.waitUntil(
         caches.open('my-cache').then(function (cache) {
             return cache.addAll(['./index.html', './index.js'])
